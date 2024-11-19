68f9689234b5ded1eb9b69b212d7c32673a829ce
zariiii9003 <52598363+zariiii9003@users.noreply.github.com>
2024-11-19 19:06:03 +0100

    Implement E2E Profile 7 (#11)

--- src/e2e/__init__.py
+++ src/e2e/__init__.py
@@ -5,6 +5,7 @@ __all__ = [
     "p02",
     "p04",
     "p05",
+    "p07",
 ]
 
 from e2e import crc as crc
@@ -12,4 +13,5 @@ from e2e import p01 as p01
 from e2e import p02 as p02
 from e2e import p04 as p04
 from e2e import p05 as p05
+from e2e import p07 as p07
 from e2e._version import __version__
